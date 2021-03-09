7d7333cb2cbf973ecf7b98402c9b7aeb57f8b797
anzalmohamed <anzaldidah@gmail.com>
2021-03-09 10:36:06 +0300

    made changes on file

--- js/scripts.js
+++ js/scripts.js
@@ -284,17 +284,3 @@ $(document).ready(function(){
     $("#pepperoniImage").slideDown('1500');
   });
 });
-$(document).ready(function(){
-  $("form").submit(function(event){
-    event.preventDefault();
-    var name = $("input#name").val();
-    var email = $("input#email").val();
-    var message = $("textarea#comment").val();
-    if ($("input#name").val() && $("input#email").val()){
-      alert (name + ", we have received your message. Thank you for reaching out to us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
-  });
-});
